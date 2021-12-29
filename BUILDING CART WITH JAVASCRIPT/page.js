763bb138bc0a3e6e33faf08e6342a7b9ae8d8a9f
DheklanInTech <86550828+DheklanInTech@users.noreply.github.com>
2021-12-29 18:21:16 +0100

    Add files via upload

---
+++ BUILDING CART WITH JAVASCRIPT/page.js
@@ -0,0 +1,33 @@
+// first is to declare all variables
+
+let cartBtn = document.querySelector('.cart-btn');
+let closeCartBtn = document.querySelector('.close-cart');
+let clearCartBtn = document.querySelector('.clear-cart');
+let cartDOM = document.querySelector('.cart');
+let cartOverlay = document.querySelector('.cart-overlay');
+let cartItems = document.querySelector('.cart-items');
+let cartTotal = document.querySelector('.cart-total');
+let cartContent = document.querySelector('.cart-content');
+let productsDOM = document.querySelector('.products-center');
+
+//  cart
+let cart = [];
+
+// getting the products
+class Products{
+
+}
+
+// display products
+class UI{
+
+}
+// local storage
+class storage{
+    
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    let ui = new UI();
+    let products = new products();
+})
